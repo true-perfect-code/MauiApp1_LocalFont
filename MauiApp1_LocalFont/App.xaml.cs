5042b92d492a4ea9eba57eb7dc681c72af5265bf
perfe <perfe@DESKTOP-M243O9K>
2023-03-28 08:49:55 +0200

    Projektdateien hinzufügen.

---
+++ MauiApp1_LocalFont/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp1_LocalFont;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiApp1_LocalFont/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApp1_LocalFont;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
